1380ec91b2503d176dca75ff76cf2c5702996cc5
dragolea <dragolea@yahoo.com>
2024-01-24 09:35:34 +0000

    changes

--- .devcontainer/scripts/install-dependencies.sh
+++ .devcontainer/scripts/install-dependencies.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
 npm install -g @sap/cds-dk
+npm install -g npm
+npm install -g mbt@1.2.24
 npm install
 npm start
--- srv/controller/service-A/handler/Entity1Handler.ts
+++ srv/controller/service-A/handler/Entity1Handler.ts
@@ -1,8 +1,14 @@
-import { EntityHandler, Inject, SRV, Service } from '@dxfrontier/cds-ts-dispatcher';
+import { AfterRead, EntityHandler, Inject, SRV, Service, TypedRequest } from '@dxfrontier/cds-ts-dispatcher';
 import { Entity1 } from '../../../../@cds-models/ServiceA';
 
 @EntityHandler(Entity1)
 export class Entity1Handler {
   @Inject(SRV) private readonly srv: Service;
   // @OnRead, @BeforeRead, @AfterRead ...
+
+  @AfterRead()
+  public async afterRead(results: Entity1, req: TypedRequest<Entity1>): Promise<Entity1> {
+    // TODO: ...
+    return results;
+  }
 }
